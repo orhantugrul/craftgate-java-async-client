ddd5681283cbaf4a7a89c0ae37ad592f4c92df26
orhantugrul <orhantugrulsahin@gmail.com>
2025-07-04 00:41:00 +0300

    docs: add package-info for HTTP client utilities and communication classes

---
+++ lib/src/main/java/io/craftgate/http/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * HTTP client utilities and communication classes.
+ * <p>
+ * This package contains HTTP-related utilities and classes that handle communication with the
+ * Craftgate API endpoints.
+ * <p>
+ * Key classes include:
+ * <ul>
+ *   <li>{@link io.craftgate.http.HttpClient} - Main HTTP client for API communication</li>
+ *   <li>{@link io.craftgate.http.HttpUtil} - HTTP utility functions and helpers</li>
+ *   <li>{@link io.craftgate.http.HttpException} - Exception thrown for HTTP-related errors</li>
+ * </ul>
+ */
+package io.craftgate.http;
