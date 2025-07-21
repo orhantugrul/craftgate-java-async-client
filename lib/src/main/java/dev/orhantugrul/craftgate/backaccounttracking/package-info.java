2b2b576068e5801edc4b4f14e7c38cb0ec4220d9
orhantugrul <orhantugrulsahin@gmail.com>
2025-07-21 04:19:12 +0300

    docs: improve formatting in package-info files for back account tracking and HTTP client utilities

--- lib/src/main/java/dev/orhantugrul/craftgate/backaccounttracking/package-info.java
+++ lib/src/main/java/dev/orhantugrul/craftgate/backaccounttracking/package-info.java
@@ -2,7 +2,7 @@
  * Back account tracking classes and utilities for the Craftgate SDK.
  *
  * <p>
- *   This package contains classes and utilities for back account tracking.
+ * This package contains classes and utilities for back account tracking.
  * </p>
  */
 package dev.orhantugrul.craftgate.backaccounttracking;
--- lib/src/main/java/dev/orhantugrul/craftgate/http/package-info.java
+++ lib/src/main/java/dev/orhantugrul/craftgate/http/package-info.java
@@ -1,5 +1,6 @@
 /**
  * HTTP client utilities and communication classes.
+ * 
  * <p>
  * This package contains HTTP-related utilities and classes that handle communication with the
  * Craftgate API endpoints.
